409d8eb1de892bcb1ca5846d3173f1fbab9e30e0
Anthony Williams <hi@antw.io>
2016-10-07 09:52:15 +0100

    Add placeholder dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div className="dashboard">
+    <div className="dashboard-item">A</div>
+    <div className="dashboard-item">B</div>
+    <div className="dashboard-item">C</div>
+    <div className="dashboard-item">D</div>
+  </div>
+);
+
+export default Dashboard;
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import InputList from '../components/InputList';
+import Dashboard from '../components/Dashboard';
+
 import inputs from '../data/inputs';
 
 const Root = () => (
-  <InputList inputs={inputs} />
+  <div>
+    <InputList inputs={inputs} />
+    <Dashboard />
+  </div>
 );
 
 export default Root;
