156cdf467b0b717bb01216f50ce3aec919ce853c
Anthony Williams <hi@antw.io>
2016-11-02 16:48:35 +0000

    Add some more questions; replace hydro option

--- src/data/questions.js
+++ src/data/questions.js
@@ -32,22 +32,22 @@ const questions = [
   },
   {
     code: 'batteries_or_hydro',
-    name: 'Batteries or hydro?',
+    name: 'Batteries or LED lightbulbs?',
     inputs: [
-      'number_of_energy_power_combined_cycle_coal',
-      'number_of_energy_power_wind_turbine_offshore'
+      'households_flexibility_p2p_electricity_market_penetration',
+      'households_lighting_led_electricity_share'
     ],
     choices: [
       {
-        name: 'Build a hydroelectric dam',
-        icon: 'hydro',
-        values: [3.0, 73.4]
+        name: 'Store electricity in batteries',
+        icon: 'battery',
+        values: [100, 0.02]
       },
       {
-        name: 'Install batteries in a million homes',
-        icon: 'battery',
+        name: 'Use LED light bulbs',
+        icon: 'ledLighting',
         isCorrect: true,
-        values: [0.31, 871.3]
+        values: [0, 100]
       }
     ],
     description: {
@@ -56,6 +56,38 @@ const questions = [
           In painting, you have unlimited power. You have the ability to move
           mountains. You can bend rivers. But when I get home, the only thing I
           have power over is the garbage.
+        </p>
+      `
+    }
+  },
+  {
+    code: 'electric_vehicles_or_trains',
+    name: 'Travel by electric car or by train?',
+    inputs: [
+      'transport_car_using_electricity_share',
+      'transport_useful_demand_car_kms',
+      'transport_useful_demand_trains'
+    ],
+    choices: [
+      {
+        name: 'Add one million electric cars',
+        icon: 'electricVehicle',
+        isCorrect: true,
+        values: [0.1, 0, 0]
+      },
+      {
+        name: '5% of car journeys instead done by train',
+        icon: 'train',
+        values: [0.1, -5, 5]
+      }
+    ],
+    description: {
+      __html: `
+        <p>
+          Mix up a little more shadow color here, then we can put us a little
+          shadow right in there. See how you can move things around? You have
+          unlimited power on this canvas – you can literally, literally move
+          mountains
         </p>
       `
     }
--- src/images/choices/index.js
+++ src/images/choices/index.js
@@ -3,5 +3,7 @@ export { default as battery } from './battery.svg';
 export { default as co2 } from './co2.svg';
 export { default as coin } from './coin.svg';
 export { default as coal } from './coal.svg';
-export { default as hydro } from './hydro.svg';
+export { default as electricVehicle } from './electric_vehicle.svg';
+export { default as ledLighting } from './led_lighting.svg';
+export { default as train } from './train.svg';
 export { default as wind } from './wind.svg';
