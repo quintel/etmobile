a1c54225c7d48e4d3bd73c65da1e11ace83598fe
Anthony Williams <hi@antw.io>
2016-12-21 17:56:06 +0000

    Move Firebase settings to .env

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,9 +1,20 @@
 import Rebase from 're-base';
 
+/* istanbul ignore next */
+const assertEnv = (key) => {
+  const value = process.env[key];
+
+  if (!value || !value.trim().length) {
+    throw new Error(`Missing ${key} from .env`);
+  }
+
+  return value;
+};
+
 const base = Rebase.createClass({
-  apiKey: 'AIzaSyDLz5PdZ7TeBvdseDinhcTerg5fSk-lAjU',
-  authDomain: 'etmobile-beta.firebaseapp.com',
-  databaseURL: 'https://etmobile-beta.firebaseio.com'
+  apiKey: assertEnv('REACT_APP_FIREBASE_API_KEY'),
+  authDomain: assertEnv('REACT_APP_FIREBASE_AUTH_DOMAIN'),
+  databaseURL: assertEnv('REACT_APP_FIREBASE_DATABASE_URL')
 });
 
 export default base;
