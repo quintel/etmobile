34e61d067e2d0d28885937d932387713d822d87f
Anthony Williams <hi@antw.io>
2016-12-22 16:10:06 +0000

    Header links back to the front page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Header = () => <header>ETMobile - Reduce your CO2 emissions</header>;
+// <a /> should be a <Link /> to avoid reloading the page, but avoids having to
+// wrap many tests in a <MemoryRouter />
+const Header = () => (
+  <header>
+    <a href="/">ETMobile - Reduce your CO2 emissions</a>
+  </header>
+);
 
 export default Header;
