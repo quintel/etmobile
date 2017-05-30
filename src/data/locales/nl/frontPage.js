31cd473b54caa5d6f6a71e12b01ef874f91bc436
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2017-05-30 13:39:00 +0200

    indentation fix

--- src/data/locales/nl/frontPage.js
+++ src/data/locales/nl/frontPage.js
@@ -5,37 +5,37 @@ export default {
   etmDescription: `
     <p>
       Het klimaat verandert door menselijk toedoen. Dit wordt veroorzaakt door
-	  de uitstoot (emissies) van broeikasgassen. De bekendste daarvan is
-	  kooldioxide (CO<sub>2</sub>), dat ontstaat bij de verbranding van de
-	  fossiele brandstoffen steenkool, olie en aardgas. Het staat vast dat we om
-	  het klimaatvraagstuk aan te pakken uiteindelijk van deze energiebronnen af
-	  moeten en alleen duurzame alternatieven zoals wind, zon en aardwarmte 
-	  mogen inzetten.
-	  Helaas dat kan niet van de ene op de andere dag. We zijn daarvoor gewoon nog
-	  te afhankelijk van fossiele brandstoffen voor onze energievoorziening en,
-	  dus, welvaart.
+      de uitstoot (emissies) van broeikasgassen. De bekendste daarvan is
+      kooldioxide (CO<sub>2</sub>), dat ontstaat bij de verbranding van de
+      fossiele brandstoffen steenkool, olie en aardgas. Het staat vast dat we om
+      het klimaatvraagstuk aan te pakken uiteindelijk van deze energiebronnen af
+      moeten en alleen duurzame alternatieven zoals wind, zon en aardwarmte
+      mogen inzetten.
+      Helaas dat kan niet van de ene op de andere dag. We zijn daarvoor gewoon nog
+      te afhankelijk van fossiele brandstoffen voor onze energievoorziening en,
+      dus, welvaart.
     </p>
     <p>
-	  Daarom is het van groot belang dat we ons in de
-	  transitie naar een CO<sub>2</sub>-neutrale energievoorziening concentreren
-	  op de kern van het probleem: CO<sub>2</sub>-emissies. Dat betekent dat we
-	  scherp in de gaten moeten houden welke opties op dit punt het meeste
-	  opleveren. Vaak is het verstandig direct voor hernieuwbare bronnen te
-	  kiezen. Op korte termijn is het goed de meest vervuilende fossiele
-	  brandstoffen, denk aan kolen, te vervangen door schonere, met name
-	  aardgas. En spaarzamer energie consumeren is natuurlijk per definitie het
-	  slimst. Niets is schoner dan niet-gebruikte energie.
+      Daarom is het van groot belang dat we ons in de
+      transitie naar een CO<sub>2</sub>-neutrale energievoorziening concentreren
+      op de kern van het probleem: CO<sub>2</sub>-emissies. Dat betekent dat we
+      scherp in de gaten moeten houden welke opties op dit punt het meeste
+      opleveren. Vaak is het verstandig direct voor hernieuwbare bronnen te
+      kiezen. Op korte termijn is het goed de meest vervuilende fossiele
+      brandstoffen, denk aan kolen, te vervangen door schonere, met name
+      aardgas. En spaarzamer energie consumeren is natuurlijk per definitie het
+      slimst. Niets is schoner dan niet-gebruikte energie.
     </p>
     <p>
       Met dit spel onderzoek je welke opties het beste scoren op uitstoot-
-	  besparing. 
-	  Je krijgt er steeds twee voorgeschoteld. Welke brengt de
-	  CO<sub>2</sub>-uitstoot het meest naar beneden? Na beantwoording van een
-	  vraag kun je door op het vraagteken boven in het scherm te tikken te weten
-	  komen waarom de ene optie beter scoort dan de andere. Eén spelronde
-	  bestaat uit maximaal 20 vragen, als je alle antwoorden goed hebt. 
-	  Tot slot kun je je naam invullen op het scorebord. 
-	  Welk kennisniveau heb jij? Normaal, bovengemiddeld of ben je een expert?
+      besparing.
+      Je krijgt er steeds twee voorgeschoteld. Welke brengt de
+      CO<sub>2</sub>-uitstoot het meest naar beneden? Na beantwoording van een
+      vraag kun je door op het vraagteken boven in het scherm te tikken te weten
+      komen waarom de ene optie beter scoort dan de andere. Eén spelronde
+      bestaat uit maximaal 20 vragen, als je alle antwoorden goed hebt.
+      Tot slot kun je je naam invullen op het scorebord.
+      Welk kennisniveau heb jij? Normaal, bovengemiddeld of ben je een expert?
     </p>
   `,
 
