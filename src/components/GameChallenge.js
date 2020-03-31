8a722fcd051d976c616b6e0963c005aadc9bf6f5
Anthony Williams <hi@antw.dev>
2020-03-31 15:04:37 +0100

    Use prop-types

--- src/components/GameChallenge.js
+++ src/components/GameChallenge.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Game from './Game';
 import * as gameModes from '../data/gameModes';
--- src/components/GlobalLeaderBoards.js
+++ src/components/GlobalLeaderBoards.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { FormattedMessage } from 'react-intl';
 
